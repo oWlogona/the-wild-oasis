cb039e3f78e8d36c54ceab7f3f7c32214d3f3406
Andrew <artemdanilenko1997@gmail.com>
2025-02-27 21:00:37 +0200

    add: Supabase integration

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://fmlqpkuhijnztfjxfgtz.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtbHFwa3VoaWpuenRmanhmZ3R6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2Nzg2NjksImV4cCI6MjA1NjI1NDY2OX0.qjyV0z_vlFyQEE3RE_VQnv4_uwx9JKGVs_jB02GTlgc";
+const supabaseKey = "";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
