f27e5c5c78dcb089306587f728f4adf6c4dc659a
Andrew <artemdanilenko1997@gmail.com>
2025-04-09 18:51:57 +0300

    add: Authentication

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import styled from "styled-components";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  const { isAuthenticated, isLoading } = useUser();
+
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
