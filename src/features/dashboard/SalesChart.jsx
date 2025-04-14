85178542e8dd0c52aa32d85132b545207265f679
Andrew <artemdanilenko1997@gmail.com>
2025-04-14 22:51:06 +0300

    add: DashBoard chart and error hadnling

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 import DashboardBox from "./DashboardBox";
+import Heading from "../../ui/Heading";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { eachDayOfInterval, format, isSameDay, subDays } from "date-fns";
 
 const StyledSalesChart = styled(DashboardBox)`
   grid-column: 1 / -1;
@@ -57,3 +68,57 @@ const colors = isDarkMode
       text: "#374151",
       background: "#fff",
     };
+
+function SalesChart({ bookings, numDays }) {
+  const allDays = eachDayOfInterval({
+    start: subDays(new Date(), numDays - 1),
+    end: new Date(),
+  });
+
+  const data = allDays.map((date) => {
+    return {
+      label: format(date, "MMM dd"),
+      totalSales: bookings
+        .filter((booking) => isSameDay(date, new Date(booking.created_at)))
+        .reduce((acc, cur) => acc + cur.totalPrice, 0),
+      extrasSales: bookings
+        .filter((booking) => isSameDay(date, new Date(booking.created_at)))
+        .reduce((acc, cur) => acc + cur.extraPrice, 0),
+    };
+  });
+
+  return (
+    <StyledSalesChart>
+      <ResponsiveContainer height={300} width="100%">
+        <Heading as="h2">
+          Sales from {format(allDays.at(0), "MMM dd yyyy")} &mdash;{" "}
+          {format(allDays.at(-1), "MMM dd yyyy")}{" "}
+        </Heading>
+        <AreaChart data={data}>
+          <XAxis dataKey="label" />
+          <YAxis unit="$" />
+          <CartesianGrid strokeDasharray="4" />
+          <Tooltip />
+          <Area
+            dataKey="totalSales"
+            type="monotone"
+            stroke="red"
+            fill="orange"
+            strokeWidth={2}
+            name="Total sales"
+          />
+          <Area
+            dataKey="extrasSales"
+            type="monotone"
+            stroke="red"
+            fill="orange"
+            strokeWidth={2}
+            name="Extras sales"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </StyledSalesChart>
+  );
+}
+
+export default SalesChart;
